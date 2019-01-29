48569b9d5c6d31165a1762e6022f9956b89c2708
Lee-Dong-Wook <39697127+Dongwook-Raynor@users.noreply.github.com>
2019-01-29 22:46:50 +0900

    C++ study
    1. 프로젝트 관리
    2. 변수
    3. cin,cout,endl

---
+++ C++ Study/Project2/test_project2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std; 
+int main()
+{
+	std::cout << "안녕하세요" << endl; 
+	cout << "안녕하세요\n";
+	cout << "abc" << "\t" << "bcd" << endl;
+	cout << "abcd" << "\t" << "ef" << endl; 
+	cout << "\a"; 
+
+	int x; 
+	cin >> x; 
+	cout << "x의 입력 값은? : " << x << endl; 
+}
