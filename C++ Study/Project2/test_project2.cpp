15091619ef1678b3c8891b4b166e4b06db82dea9
Lee-Dong-Wook <39697127+Dongwook-Raynor@users.noreply.github.com>
2019-01-31 18:30:54 +0900

    C++ Studying
    1. 디버깅 방법
    2. 지역변수
    3. 디버깅에서 함수는 어떻게 처리되는가?

--- C++ Study/Project2/test_project2.cpp
+++ C++ Study/Project2/test_project2.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 using namespace std; 
+
+int sum(int a, int b) {
+	
+	int result = a + b; 
+
+	return result; 
+}
+
 int main()
 {
 	std::cout << "안녕하세요" << endl; 
@@ -8,7 +16,12 @@ int main()
 	cout << "abcd" << "\t" << "ef" << endl; 
 	cout << "\a"; 
 
-	int x; 
-	cin >> x; 
-	cout << "x의 입력 값은? : " << x << endl; 
-}
+	cout << sum(1,2)<< endl;
+	cout << sum(3, 4)<< endl;
+
+	int x, y;
+	cout << "수를 입력하세요";
+	cin >> x >> y; 
+
+	cout << sum(x,y);
+} 
